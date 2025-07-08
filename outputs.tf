ceb7dce5f9b255512a425c00c9f9b708a66e3b0e
bacem-g <ghali.bacem@gmail.com>
2025-07-08 16:41:45 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,7 @@
 output "vpc_id" {
   value = aws_vpc.main.id
 }
+
 output "subnet_ids" {
   value = [
     aws_subnet.public-subnet-a.id,
@@ -8,12 +9,15 @@ output "subnet_ids" {
     aws_subnet.private-subnet-a.id,
   aws_subnet.private-subnet-b.id]
 }
+
 output "public_subnet_ids" {
   value = [aws_subnet.public-subnet-a.id, aws_subnet.public-subnet-b.id]
 }
+
 output "private_subnet_ids" {
   value = [aws_subnet.private-subnet-a.id, aws_subnet.private-subnet-b.id]
 }
+
 output "route53_id" {
   value = aws_route53_zone.private-zone.zone_id
 }
